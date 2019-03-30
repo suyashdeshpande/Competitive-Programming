ee4a00b254b7a4314406ad7a072607f5130a4e4b
Suyash Deshpande <suyash.champ1@gmail.com>
2019-03-30 16:29:37 +0530

    updated template

--- competitive_template.cpp
+++ competitive_template.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
-#define SIEVE_SIZE 10000
 using namespace std;
+
+#define SIEVE_SIZE 10000
+#define ll long long
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define mod 1000000007
+#define inf 999999999999999999
 
 //prime sieve for a sieve size SIEVE_SIZE
 vector<bool> sieve(SIEVE_SIZE, true);
