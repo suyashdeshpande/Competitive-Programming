3a5ac2a8e06cad648d94b5d1213c74ca31b4d4c6
Suyash Deshpande <suyash.champ1@gmail.com>
2019-10-16 08:01:47 +0530

    133-LeetCode

---
+++ LeetCode/133.cpp
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+class Solution {
+private:
+    unordered_map<Node*, Node*> copies;
+    
+public:
+    
+    Node* cloneGraph(Node* node) {
+        if (!node) {
+            return NULL;
+        }
+        if (copies.find(node) == copies.end()) {
+            copies[node] = new Node(node -> val, {});
+            for (Node* neighbor : node -> neighbors) {
+                copies[node] -> neighbors.push_back(cloneGraph(neighbor));
+            }
+        }
+        return copies[node];
+    }
+
+};
