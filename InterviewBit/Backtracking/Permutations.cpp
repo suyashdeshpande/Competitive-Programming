b199b3083a0671ef9099ea6c7fc09462ab266442
Suyash Deshpande <suyash.champ1@gmail.com>
2019-10-26 01:08:39 +0530

    Permutations-InterviewBit

---
+++ InterviewBit/Backtracking/Permutations.cpp
@@ -0,0 +1,18 @@
+void permutation(vector<int> &A, int st, vector<vector<int> > &v) {
+    if(st >= A.size()) {
+        v.push_back(A);
+        return;
+    }
+    for(int i = st; i < A.size(); i++)
+    {
+        swap(A[st], A[i]);
+        permutation(A, st + 1, v);
+        swap(A[st], A[i]);
+    }
+}
+
+vector<vector<int> > Solution::permute(vector<int> &A)  {
+    vector<vector<int> > v;
+    permutation(A, 0, v);
+    return v;
+}
